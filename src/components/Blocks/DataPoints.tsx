3530cbe425778fd44cd93c2de5eee63650b1e7d2
syed zunair gillani <gillanix007@gmail.com>
2023-08-29 13:19:47 +0500

    update

--- src/components/Blocks/DataPoints.tsx
+++ src/components/Blocks/DataPoints.tsx
@@ -95,8 +95,8 @@ export const DataPoints = ({
             <ImageWrapper>
               <NextImage
                 {...getImageAttributes(block.image)}
-                width={getImageAttributes(block.image).width}
-                height={getImageAttributes(block.image).height}
+                width={parseInt(getImageAttributes(block.image).width)}
+                height={parseInt(getImageAttributes(block.image).height)}
                 objectFit="cover"
                 quality="100"
                 priority
